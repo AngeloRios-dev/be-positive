3ce26311573b860f8cbc5db28a58d591e6410966
Angelo Rios <angelorios@live.com>
2024-03-21 18:36:46 +0100

    fix: restructurado, evitando codigo duplicado

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -1,12 +1,22 @@
-// seleccionando los elementos a del formulario
-// para validar formulario:
-const nombre = document.getElementById('inputNombre');
-const apellidos = document.getElementById('inputApellidos');
-const email = document.getElementById('inputEmail');
-const telefono = document.getElementById('inputTelefono');
+// Objeto para almacenar elementos del formulario
+const formElements = {
+    nombre: document.getElementById('inputNombre'),
+    apellidos: document.getElementById('inputApellidos'),
+    email: document.getElementById('inputEmail'),
+    telefono: document.getElementById('inputTelefono'),
+    material: document.getElementById('tipoMaterial'),
+    plazoEnvio: document.getElementById('plazoEnvio'),
+    textoDisenio: document.getElementById('textoDisenio'),
+    colorPersonalizado: document.getElementById('colorPersonalizado'),
+    tipografiaPersonalizada: document.getElementById('tipografiaPersonalizada'),
+    donacionCaridad: document.getElementById('donacionCaridad'),
+    cantidadCamisetas: document.getElementById('cantidadCamisetas'),
+    presupuestoEstimado: document.getElementById('presupuestoEstimado'),
+    aceptarTerminos: document.getElementById('aceptarTerminos'),
+    solicitarPresupuesto: document.getElementById('solicitarPresupuesto'),
+};
 
-
-// funcion para indicar el error
+// Funcion para indicar el error en un campo
 const setError = (elemento, mensaje) => {
     const mostrarMensaje = elemento.nextElementSibling;
     const setBorde = elemento;
@@ -16,8 +26,7 @@ const setError = (elemento, mensaje) => {
     setBorde.classList.remove('exito');
 }
 
-
-// indicar que el valor es correcto
+// Funcion para indicar que el valor de un campo es correcto
 const setExito = (elemento) => {
     const eliminarMensaje = elemento.nextElementSibling;
     const setBorde = elemento;
@@ -27,138 +36,101 @@ const setExito = (elemento) => {
     setBorde.classList.remove('error');
 }
 
-
-const validarNombre = (nombre) => {
-    // Expresion para verificar que la longitud cumpla con lo espesificado
+// Funcion para validar el nombre
+const validarNombre = () => {
+    const nombre = formElements.nombre;
     const patron = /^[a-zA-Z]{4,15}$/;
 
-    // comprobar que el valor coincida con el patron
     if (patron.test(nombre.value)) {
-        // marcar como correcto si es correcto
-        setExito(nombre)
+        setExito(nombre);
+        return true;
     } else {
-        // de ser invalido, mostrar mensaje de error
-        setError(nombre, 'El nombre debe tener entre 4 y 15 caracteres alfabeticos.')
+        setError(nombre, 'El nombre debe tener entre 4 y 15 caracteres alfabeticos.');
+        return false;
     }
 }
 
-
-const validarApellidos = (apellidos) => {
-    // Expresion para verificar que la longitud cumpla con lo espesificado
+// Funcion para validar los apellidos
+const validarApellidos = () => {
+    const apellidos = formElements.apellidos;
     const patron = /^[a-zA-Z]{4,40}$/;
 
-    // comprobar que el valor coincida con el patron
     if (patron.test(apellidos.value)) {
-        // marcar como correcto si es correcto
-        setExito(apellidos)
+        setExito(apellidos);
+        return true;
     } else {
-        // de ser invalido, mostrar mensaje de error
-        setError(apellidos, 'El nombre debe tener entre 4 y 40 caracteres alfabeticos.')
+        setError(apellidos, 'Los apellidos deben tener entre 4 y 40 caracteres alfabeticos.');
+        return false;
     }
 }
 
-
-const validarEmail = (email) => {
+// Funcion para validar el email
+const validarEmail = () => {
+    const email = formElements.email;
     const patron = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    // comprobar que el valor coincida con el patron
     if (patron.test(email.value)) {
-        // marcar como correcto si es correcto
         setExito(email);
+        return true;
     } else {
-        // de ser invalido, mostrar mensaje de error
-        setError(email, 'Por favor, introducta un correo valido. Ej: nnnnn_nnn@zzzz.xxx')
+        setError(email, 'Por favor, introduce un correo valido.');
+        return false;
     }
 }
 
-const validarTelefono = (telefono) => {
+// Funcion para validar el telefono
+const validarTelefono = () => {
+    const telefono = formElements.telefono;
     const patron = /^\d{9}$/;
 
-    // comprobar que el valor coincida con el patron
     if (patron.test(telefono.value)) {
-        // marcar como correcto si es correcto
         setExito(telefono);
+        return true;
     } else {
-        // de ser invalido, mostrar mensaje de error
-        setError(telefono, 'Por favor, introduzca un numero de 9 digitos.');
+        setError(telefono, 'Por favor, introduce un número de telefono valido.');
+        return false;
     }
 }
 
+// indicar al usuario que debe verificar algun campo faltante
+const indicarVerificar = () => {
+    const enviarSolicitud = formElements.solicitarPresupuesto;
+    setError(enviarSolicitud, 'Por favor, verificar que todos los campos esten cumplimentados.');
+} 
 
-// agregar un evento al campo nombre para validar lo introducido
-nombre.addEventListener('blur', function() {
-    validarNombre(nombre);
-});
-
-// agregar un evento al campo apellidos para validar lo introducido
-apellidos.addEventListener('blur', function() {
-    validarApellidos(apellidos);
-});
-
-// agregar un evento al campo email para validar lo introducido
-email.addEventListener('blur', function() {
-    validarEmail(email);
-});
-
-
-// agregar un evento al campo para validar lo introducido
-telefono.addEventListener('blur', function() {
-    validarTelefono(telefono)
-});
-
-// ************************************************
-// WIP: REORGANIZAR AL FINALIZAR ESTA PARTE
-// WIP: REORGANIZAR AL FINALIZAR ESTA PARTE
-// ************************************************
-// MOVER ESTO AL PRINCIPIO CUANDO TERMINE:
-// constantes para gestionar el valor del presupuesto 
-const material = document.getElementById('tipoMaterial');
-const plazoEnvio = document.getElementById('plazoEnvio');
-const textoDisenio = document.getElementById('textoDisenio');
-const colorPersonalizado = document.getElementById('colorPersonalizado');
-const tipografiaPersonalizada = document.getElementById('tipografiaPersonalizada');
-const donacionCaridad = document.getElementById('donacionCaridad');
-const cantidadCamisetas = document.getElementById('cantidadCamisetas');
-const presupuestoEstimado = document.getElementById('presupuestoEstimado');
-
-// funcion para calcular el descuento
+// Funcion para calcular el descuento en el presupuesto
 const calcularDescuento = (subTotal) => {
-    let costoEnvio = parseFloat(plazoEnvio.value);
+    let descuento = 0;
+    const costoEnvio = parseFloat(formElements.plazoEnvio.value);
     if (!isNaN(costoEnvio) && costoEnvio > 1) {
-        descuento = subTotal - (subTotal * costoEnvio)
-        console.log(descuento);
-        return descuento; 
-    } else {
-        return 0;
+        descuento = subTotal - (subTotal * costoEnvio);
+    }
+    return descuento;
+}
+
+// Funcion para verificar la cantidad mínima de camisetas
+const verificarCantidad = () => {
+    const cantidad = parseFloat(formElements.cantidadCamisetas.value);
+    const materialSeleccionado = formElements.material.value;
+    if (materialSeleccionado !== 'Elige...' && isNaN(cantidad)) {
+        formElements.cantidadCamisetas.value = 1;
     }
 }
 
-
-// verificar cantidad minima de camisetas
-const verificarCantidad = () => {
-    let cantidad = parseFloat(cantidadCamisetas.value);
-    let materialSeleccionado = material.value
-    if (materialSeleccionado !== 'Elige...' && isNaN(cantidad)) {
-        cantidadCamisetas.value = 1;
-    } 
-}
-
-// funcion para calcular el presupuesto
-const calculaPresupeusto = () => {
+// Funcion para calcular el presupuesto
+const calcularPresupuesto = () => {
     verificarCantidad();
     let subTotal = 0;
-    let descuento = 0;
 
-    // Obtener el valor del los elementos en el formulario
-    let valorMaterial = parseFloat(material.value);
-    let costoDisenio = textoDisenio.checked ? parseFloat(textoDisenio.value) : 0;
-    let costoColor = colorPersonalizado.checked ? parseFloat(colorPersonalizado.value) : 0;
-    let costoTipografia = tipografiaPersonalizada.checked ? parseFloat(tipografiaPersonalizada.value) : 0;
-    let donacion = donacionCaridad.checked ? parseFloat(donacionCaridad.value) : 0;
-    let cantidad = parseFloat(cantidadCamisetas.value);
+    // Obtener los valores de los elementos del formulario
+    const valorMaterial = parseFloat(formElements.material.value);
+    const costoDisenio = formElements.textoDisenio.checked ? parseFloat(formElements.textoDisenio.value) : 0;
+    const costoColor = formElements.colorPersonalizado.checked ? parseFloat(formElements.colorPersonalizado.value) : 0;
+    const costoTipografia = formElements.tipografiaPersonalizada.checked ? parseFloat(formElements.tipografiaPersonalizada.value) : 0;
+    const donacion = formElements.donacionCaridad.checked ? parseFloat(formElements.donacionCaridad.value) : 0;
+    const cantidad = parseFloat(formElements.cantidadCamisetas.value);
 
-    // agregar el valor a la variable presupuesto 
-    // antes comprobando que sea numerico
+    // Agregar los valores al subtotal
     if (!isNaN(valorMaterial)) subTotal += valorMaterial;
     if (!isNaN(costoDisenio)) subTotal += costoDisenio;
     if (!isNaN(costoColor)) subTotal += costoColor;
@@ -166,28 +138,59 @@ const calculaPresupeusto = () => {
     if (!isNaN(donacion)) subTotal += donacion;
     if (!isNaN(cantidad) && cantidad >= 1) subTotal *= cantidad;
 
+    // Calcular el descuento
+    const descuento = calcularDescuento(subTotal);
 
-    descuento = calcularDescuento(subTotal);
+    // Calcular el total
+    const total = subTotal + descuento;
 
-    let total = subTotal + descuento;
-    
-    presupuestoEstimado.value = total;
+    // Mostrar el presupuesto estimado
+    formElements.presupuestoEstimado.value = total;
 }
 
+// Event listeners para validar los campos del formulario
+formElements.nombre.addEventListener('blur', validarNombre);
+formElements.apellidos.addEventListener('blur', validarApellidos);
+formElements.email.addEventListener('blur', validarEmail);
+formElements.telefono.addEventListener('blur', validarTelefono);
 
-// bucle para agregar un event on change para los campos a calcular
-const sumarCampos = [
-    material,
-    plazoEnvio,
-    textoDisenio,
-    colorPersonalizado,
-    tipografiaPersonalizada,
-    donacionCaridad,
-    cantidadCamisetas,
-    presupuestoEstimado,
+// Event listener para calcular el presupuesto cuando cambia algun campo relevante
+const camposACalcular = [
+    formElements.material,
+    formElements.plazoEnvio,
+    formElements.textoDisenio,
+    formElements.colorPersonalizado,
+    formElements.tipografiaPersonalizada,
+    formElements.donacionCaridad,
+    formElements.cantidadCamisetas,
 ];
-sumarCampos.forEach(campo => {
-    campo.addEventListener('change', () => {
-        calculaPresupeusto();
-    });
+camposACalcular.forEach(campo => {
+    campo.addEventListener('change', calcularPresupuesto);
 });
+
+const validarFormulario = (event) => {
+    event.preventDefault();
+
+    // Verificar campos de datos personales
+    if (!validarNombre() || !validarApellidos() || !validarEmail() || !validarTelefono()) {
+        indicarVerificar()
+        return;
+    }
+
+    // Verificar checkbox de aceptacion de terminos
+    if (!formElements.aceptarTerminos.checked) {
+        alert('Debes aceptar los terminos y condiciones para enviar el formulario.');
+        return;
+    }
+
+    // Si todos los campos estan completos y el checkbox esta marcado, enviar el formulario
+    if (formElements.presupuestoEstimado.value === '' || formElements.presupuestoEstimado.value === 0) {
+        alert('Nos pondremos en contacto con usted lo mas pronto posible.');
+    } else {
+        alert('Solicitud enviada. Le contactaremos pronto.');
+    }
+    document.getElementById('formPresupuesto').submit();
+} 
+
+// Agregar el event listener al submit del formulario
+document.getElementById('formPresupuesto').addEventListener('submit', validarFormulario);
